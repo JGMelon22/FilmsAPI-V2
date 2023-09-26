2a090a187c349050d544718986b87bbaf179b70b
JGMelon22 <jgvasconcellos22@gmail.com>
2023-09-26 19:55:49 -0300

    feat: creating first Genres migration

---
+++ Migrations/20230926225450_GenresFirstMigration.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace FilmsAPI_V2.Migrations;
+
+/// <inheritdoc />
+public partial class GenresFirstMigration : Migration
+{
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.CreateTable(
+            name: "Genres",
+            columns: table => new
+            {
+                GenreId = table.Column<int>(type: "integer", nullable: false)
+                    .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                GenreName = table.Column<string>(type: "text", nullable: false)
+            },
+            constraints: table =>
+            {
+                table.PrimaryKey("PK_Genres", x => x.GenreId);
+            });
+    }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropTable(
+            name: "Genres");
+    }
+}
