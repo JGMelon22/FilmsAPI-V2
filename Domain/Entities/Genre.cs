5fbfd6bc70490b13f32e5ad1545f3b690a0b86e9
JGMelon22 <jgvasconcellos22@gmail.com>
2023-09-26 19:53:49 -0300

    feat: developing Genres entity and registering it at AppDbContext

---
+++ Domain/Entities/Genre.cs
@@ -0,0 +1,7 @@
+namespace FilmsAPI_V2.Domain.Entities;
+
+public class Genre
+{
+    public int GenreId { get; set; }
+    public String GenreName { get; set; } = null!; 
+}
--- Infrastructure/Data/AppDbContext.cs
+++ Infrastructure/Data/AppDbContext.cs
@@ -1,3 +1,4 @@
+using FilmsAPI_V2.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace FilmsAPI_V2.Infrastructure.Data;
@@ -7,4 +8,6 @@ public class AppDbContext : DbContext
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     {
     }
+
+    public DbSet<Genre> Genres => Set<Genre>();
 }
