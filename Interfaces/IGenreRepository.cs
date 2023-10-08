53b1c7a3e88c13e7c0479f8b323c843d9ceb2ffa
JGMelon22 <jgvasconcellos22@gmail.com>
2023-10-08 13:43:55 -0300

    chore: developing Genre Reposiotory Interface

---
+++ Interfaces/IGenreRepository.cs
@@ -0,0 +1,10 @@
+namespace FilmsAPI_V2.Interfaces;
+
+public interface IGenreRepository
+{
+    Task<ServiceResponse<List<Genre>>> GetAllGenres();
+    Task<ServiceResponse<Genre>> GetGenreById(int id);
+    Task AddGenre(Genre genre);
+    Task<ServiceResponse<Genre>> UpdateGenre(Genre genre);
+    Task RemoveGenre(int id);
+}
