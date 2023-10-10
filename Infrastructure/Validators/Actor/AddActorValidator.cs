0b3d9a3018a8141ca41d7fd664070820eb471bc8
JGMelon22 <jgvasconcellos22@gmail.com>
2023-10-10 17:16:07 -0300

    feat: developing validation with Fluent Validation for Actors

---
+++ Infrastructure/Validators/Actor/AddActorValidator.cs
@@ -0,0 +1,36 @@
+using FilmsAPI_V2.DTOs.Actor;
+using FluentValidation;
+
+namespace FilmsAPI_V2.Infrastructure.Validators.Actor;
+
+public class AddActorValidator : AbstractValidator<AddActorDto>
+{
+    public AddActorValidator()
+    {
+        RuleFor(a => a.ActorName)
+            .NotEmpty()
+            .WithMessage("Actor Name must be informed!")
+            .NotNull()
+            .WithMessage("Actor Name must be informed!")
+            .MaximumLength(150)
+            .WithMessage("Actor Name can't exceed 150 characters!");
+
+        RuleFor(a => a.Salary)
+            .NotEmpty()
+            .WithMessage("Actor Salary must be informed!")
+            .NotNull()
+            .WithMessage("Actor Salary must be informed!")
+            .Must(salary => salary >= 1.0M)
+            .WithMessage($"Actor Salary out of range!")
+            .Must(salary => salary <= 9999999.99M)
+            .WithMessage($"Actor Salary out of range!");
+
+        RuleFor(a => a.BirthDate)
+            .NotEmpty()
+            .WithMessage("Actor Birthdate must be informed!")
+            .NotNull()
+            .WithMessage("Actor Birthdate must be informed!")
+            .Must(birthDate => birthDate < DateTime.Now)
+            .WithMessage("Invalid Actor Birthdate!");
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
+using FilmsAPI_V2.DTOs.Actor;
 using FilmsAPI_V2.DTOs.Genre;
 using FilmsAPI_V2.Infrastructure.Data;
 using FilmsAPI_V2.Infrastructure.Repositories;
+using FilmsAPI_V2.Infrastructure.Validators.Actor;
 using FilmsAPI_V2.Infrastructure.Validators.Genre;
 using FilmsAPI_V2.Interfaces;
 using FluentValidation;
@@ -22,6 +24,9 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 // Fluent Validation
 builder.Services.AddFluentValidationAutoValidation();
 builder.Services.AddScoped<IValidator<AddGenreDto>, AddGenreValidator>();
+builder.Services.AddScoped<IValidator<UpdateGenreDto>, UpdateGenreValidator>();
+builder.Services.AddScoped<IValidator<AddActorDto>, AddActorValidator>();
+builder.Services.AddScoped<IValidator<UpdateActorDto>, UpdateActorValidator>();
 
 // Interface and Repositories
 builder.Services.AddScoped<IGenreRepository, GenreRepository>();
