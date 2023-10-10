c811cf4016fb48b92d8e4669a3200c961fa385ad
JGMelon22 <jgvasconcellos22@gmail.com>
2023-10-10 16:41:32 -0300

    feat: developing validation with Fluent Validation for Genres

---
+++ Infrastructure/Validators/Genre/AddGenreValidator.cs
@@ -0,0 +1,18 @@
+using FilmsAPI_V2.DTOs.Genre;
+using FluentValidation;
+
+namespace FilmsAPI_V2.Infrastructure.Validators.Genre;
+
+public class AddGenreValidator : AbstractValidator<AddGenreDto>
+{
+    public AddGenreValidator()
+    {
+        RuleFor(g => g.GenreName)
+            .NotEmpty()
+            .WithMessage("Genre Name must be informed!")
+            .NotNull()
+            .WithMessage("Genre Name must be informed!")
+            .MaximumLength(150)
+            .WithMessage("Genre Name can't exceed 150 characters long!");
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,10 @@
+using FilmsAPI_V2.DTOs.Genre;
 using FilmsAPI_V2.Infrastructure.Data;
 using FilmsAPI_V2.Infrastructure.Repositories;
+using FilmsAPI_V2.Infrastructure.Validators.Genre;
 using FilmsAPI_V2.Interfaces;
+using FluentValidation;
+using FluentValidation.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,6 +18,10 @@ builder.Services.AddSwaggerGen();
 // DbContext service 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("Default")));
+
+// Fluent Validation
+builder.Services.AddFluentValidationAutoValidation();
+builder.Services.AddScoped<IValidator<AddGenreDto>, AddGenreValidator>();
 
 // Interface and Repositories
 builder.Services.AddScoped<IGenreRepository, GenreRepository>();
