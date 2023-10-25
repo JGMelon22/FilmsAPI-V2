86cd0f936472adc68a9fa7e4a19f4a9c3555d06a
JGMelon22 <jgvasconcellos22@gmail.com>
2023-10-25 16:19:08 -0300

    chore: developing Update and Delete movie contract on IMovieRepository

--- Interfaces/IMovieRepository.cs
+++ Interfaces/IMovieRepository.cs
@@ -7,4 +7,6 @@ public interface IMovieRepository
     Task<ServiceResponse<List<GetMovieDto>>> GetAllMovies();
     Task<ServiceResponse<GetMovieDto>> GetMovieById(int id);
     Task AddMovie(AddMovieDto newMovie);
+    Task<ServiceResponse<GetMovieDto>> UpdateMovie(UpdateMovieDto updatedMovie);
+    Task RemoveMovie(int id);
 }
